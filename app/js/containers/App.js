3838dc3433f900991955fe8dcc3f079c28f01e8b
PolarisChen <polaris_chen@163.com>
2017-01-03 03:42:12 +0800

    Remove flash fallback codes

--- app/js/containers/App.js
+++ app/js/containers/App.js
@@ -13,14 +13,6 @@ class App extends React.Component {
         <figure id="videoContainer">
            <video id="video" controls preload="metadata" poster="img/poster.png">
               <source src="videos/sample.mp4" type="video/mp4" />
-
-              <object type="application/x-shockwave-flash" data="flash-player.swf?videoUrl=videos/sample.mp4" width="1024" height="576">
-                 <param name="movie" value="flash-player.swf?videoUrl=videos/sample.mp4" />
-                 <param name="allowfullscreen" value="true" />
-                 <param name="wmode" value="transparent" />
-                 <param name="flashvars" value="controlbar=over&amp;image=img/poster.png&amp;file=flash-player.swf?videoUrl=videos/sample.mp4" />
-                 <img alt="Sample poster image" src="img/poster.png" width="1024" height="428" title="No video playback possible, please download the video from the link below" />
-              </object>
            </video>
            <figcaption>&copy; Polaris Chen</figcaption>
         </figure>
