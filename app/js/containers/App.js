d93f81b22d24fdf6824b50dfb53734c38c0c2db9
PolarisChen <polaris_chen@163.com>
2017-01-03 05:20:46 +0800

    Support active cue text mark

--- app/js/containers/App.js
+++ app/js/containers/App.js
@@ -5,32 +5,43 @@ import React from 'react'
 class App extends React.Component {
 	constructor(props) {
 		super(props)
+    this.state = {
+			activeCueText: '',
+		}
 	}
   componentDidMount() {
-    var videoElement = document.querySelector("video")
-    var textTrack = videoElement.textTracks[0]
-    textTrack.oncuechange = this.handleCueChange
-    document.onkeypress = this.handleKeyPress
-  }
-  handleCueChange = function () {
-    var cue = this.activeCues[0]
-    if (cue) {
-      console.log(cue.startTime, cue.endTime, cue.text)
+    const videoElement = document.querySelector("video")
+    const textTrack = videoElement.textTracks[0]
+    const that = this
+    textTrack.oncuechange = function () {
+      const cue = this.activeCues[0]
+      if (cue) {
+        console.log(cue.startTime, cue.endTime, cue.text)
+        that.setState({activeCueText: cue.text})
+        console.log('Active cue:', that.state.activeCueText)
+      }
     }
+    document.onkeypress = this.handleKeyPress
   }
   handleKeyPress = event => {
     const firstKeyCode = 49
     const secondKeyCode = 50
     const thirdKeyCode = 51
+
+    const cueText = this.state.activeCueText
+    const length = Math.floor(cueText.length / 3)
     switch (event.keyCode) {
       case firstKeyCode:
         console.log('1 pressed')
+        console.log('Marking', cueText.substring(0, length))
         break;
       case secondKeyCode:
         console.log('2 pressed')
+        console.log('Marking', cueText.substring(length, length * 2))
         break;
       case thirdKeyCode:
         console.log('3 pressed')
+        console.log('Marking', cueText.substring(length * 2, cueText.length))
         break;
       default:
         break;
