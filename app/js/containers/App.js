b9c822bafeaa163ebd0eb822c6f22b69133bb068
PolarisChen <polaris_chen@163.com>
2017-02-13 11:14:42 +0800

    Update marking way

--- app/js/containers/App.js
+++ app/js/containers/App.js
@@ -31,26 +31,27 @@ class App extends React.Component {
       const length = Math.floor(cueText.length / 3)
       let markedIndex = 0
       let markedText = ''
-      let regx = /<c\.n1>(.*?)<\/c><c\.n2>(.*?)<\/c><c\.n3>(.*?)<\/c>/
-      let texts = cueText.match(regx);
+      // let regx = /<c\.n1>(.*?)<\/c><c\.n2>(.*?)<\/c><c\.n3>(.*?)<\/c>/
+      // let texts = cueText.match(regx);
       switch (event.keyCode) {
         case firstKeyCode:
           markedIndex = 1
-          // markedText = cueText.substring(0, length)
+          markedText = cueText.substring(0, length)
           break;
         case secondKeyCode:
           markedIndex = 2
-          // markedText = cueText.substring(length, length * 2)
+          markedText = cueText.substring(length, length * 2)
           break;
         case thirdKeyCode:
           markedIndex = 3
-          // markedText = cueText.substring(length * 2, cueText.length)
+          markedText = cueText.substring(length * 2, cueText.length)
           break;
         default:
           break;
       }
-      markedText = texts[markedIndex]
-      const content = `Cue ID: ${cueId}; Text: ${texts.join('').replace(cueText, '')}; Marked Text: ${markedText}; Marked Position: ${markedIndex}`
+      // markedText = texts[markedIndex]
+      // const content = `Cue ID: ${cueId}; Text: ${texts.join('').replace(cueText, '')}; Marked Text: ${markedText}; Marked Position: ${markedIndex}`
+			const content = `Cue ID: ${cueId}; Text: ${cueText}; Marked Text: ${markedText}; Marked Position: ${markedIndex}`
       const logText = `Marked "${markedText}" in ${markedIndex}/3 of cue ${cueId}`
       console.log('Marked', markedIndex, markedText)
       this.setState({logText: logText})
